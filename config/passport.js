508d619a2652df927f2c046fbd6ad8df8e63d6c2
rauelguimmayen <rauelguimmayen@gmail.com>
2026-05-05 16:28:05 +0800

    updated file path

--- config/passport.js
+++ config/passport.js
@@ -32,25 +32,7 @@ passport.use(new LocalStrategy(
     }
   }
 ));
-/*passport.use(new GoogleStrategy({
-  clientID: process.env.GOOGLE_CLIENT_ID,
-  clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: process.env.GOOGLE_CALLBACK_URL || "http://localhost:3000/auth/google/callback",
-  passReqToCallback: true
-},
 
-function(request, accessToken, refreshToken, profile, done){
-  return done(null, profile)
-}
-))
-
-passport.serializeUser(function(user,done){
-  done(null, user);
-});
-
-passport.deserializeUser(function(user, done){
-  done(null, user);
-});*/
 // ===== GOOGLE STRATEGY =====
 if (process.env.GOOGLE_CLIENT_ID && process.env.GOOGLE_CLIENT_ID !== 'your-google-client-id') {
   passport.use(new GoogleStrategy(
